99ab5832d0622b0a13895f2d5586e474817dc857
Ammaro173 <ammaraf97@gmail.com>
2022-02-17 00:04:15 +0200

    solved

--- my-app/src/components/Movie/Movie.js
+++ my-app/src/components/Movie/Movie.js
@@ -7,33 +7,39 @@ function MovieList(props) {
   console.log('mss', props.movies);
   const [show, setShow] = useState(false);
   const [now, setNow] = useState([]);
+  // const [movie, setMovie] = useState();
 
-  async function addToFav(ele) {
+  const addToFav = async (ele) => {
+    console.log('chk', ele);
     try {
-      const res = await fetch(`${process.env.REACT_APP_SERVER}/addmovie}`, {
+      const res = await fetch(`${process.env.REACT_APP_SERVER}/addmovie`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          title: ele.title,
+          overview: ele.overview,
           poster_path: ele.poster_path,
-          review: ele.review,
+          release_date: ele.release_date,
+          title: ele.title,
         }),
       });
       const data = await res.json();
+      console.log('baraa', data);
     } catch (error) {
       console.log('error', error);
     }
-  }
+  };
+
+  // async function addToFav(ele) {
 
   return (
     <>
       <CardGroup style={{ display: 'flex', justifyContent: 'space-around', gap: 30, bordercolor: 'black' }}>
         {(props.movies ?? []).map((movie) => {
           return (
-            <div>
+            <div key={movie.id}>
               <Card>
                 <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                 <Card.Body>
@@ -53,7 +59,14 @@ function MovieList(props) {
                   >
                     show more
                   </Button>
-                  <Button variant='dark' onClick={addToFav(props.movies)}>
+                  <Button
+                    variant='dark'
+                    onClick={() => {
+                      // setMovie(movie);
+                      // console.log('baraaaa', movie);
+                      addToFav(movie);
+                    }}
+                  >
                     add to Favourite
                   </Button>
                 </Card.Body>
--- my-app/src/components/Navbar/navbar.js
+++ my-app/src/components/Navbar/navbar.js
@@ -10,7 +10,7 @@ function NavBarss() {
         <Navbar.Brand href='/'>Netflix</Navbar.Brand>
         <Nav className='me-auto'>
           <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/FavList'>Favourite</Nav.Link>
+          <Nav.Link href='FavList'>Favourite</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
