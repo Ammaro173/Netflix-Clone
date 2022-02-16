4e7ea8ee0cce05469a8761b59067bc0f997cc1f5
Ammaro173 <ammaraf97@gmail.com>
2022-02-16 22:10:26 +0200

    task 18 + 19 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 import { Routes, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home/Home';
+import FavList from './components/FavList/FavList';
 
 const PageNotFound = () => (
   <div>
@@ -13,12 +15,29 @@ const PageNotFound = () => (
 );
 
 function App() {
+  const [favouriteList, setFavList] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`${process.env.REACT_APP_SERVER}/getmyMovies`);
+
+      const data = await res.json();
+      setFavList(data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='body'>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/FavList' element={<FavList />} /> */}
-        <Route component={PageNotFound} />
+        <Route path='/FavList' element={<FavList favouriteList={favouriteList} />} />
+        <Route path='*' component={PageNotFound} />
       </Routes>
     </div>
   );
--- my-app/src/components/Movie/Movie.js
+++ my-app/src/components/Movie/Movie.js
@@ -4,38 +4,66 @@ import ModalMovie from '../ModalMovie/ModalMovie';
 
 function MovieList(props) {
   console.log('ms', props);
+  console.log('mss', props.movies);
   const [show, setShow] = useState(false);
-  const [now, setNow] = useState(props);
+  const [now, setNow] = useState([]);
+
+  async function addToFav(ele) {
+    try {
+      const res = await fetch(`${process.env.REACT_APP_SERVER}/addmovie}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: ele.title,
+          poster_path: ele.poster_path,
+          review: ele.review,
+        }),
+      });
+      const data = await res.json();
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
   return (
     <>
       <CardGroup style={{ display: 'flex', justifyContent: 'space-around', gap: 30, bordercolor: 'black' }}>
-        {props.movies &&
-          props.movies.map((movie) => {
-            return (
-              <div>
-                <Card>
-                  <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                  <Card.Body>
-                    <Card.Title>{movie.title}</Card.Title>
-                    <Card.Text>{movie.release_date}</Card.Text>
-                    <Button
-                      variant='primary'
-                      onClick={() => {
-                        setShow(true);
-                        setNow(movie);
-                      }}
-                    >
-                      show more
-                    </Button>
-                    <Button variant='dark'>add to Favourite</Button>
-                  </Card.Body>
-                  {/* <Card.Footer>
+        {(props.movies ?? []).map((movie) => {
+          return (
+            <div>
+              <Card>
+                <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>
+                    {movie.release_date}
+                    <br />
+                    review:
+                    {movie.review}
+                  </Card.Text>
+                  <Button
+                    variant='primary'
+                    onClick={() => {
+                      setShow(true);
+                      setNow(movie);
+                    }}
+                  >
+                    show more
+                  </Button>
+                  <Button variant='dark' onClick={addToFav(props.movies)}>
+                    add to Favourite
+                  </Button>
+                </Card.Body>
+                {/* <Card.Footer>
                   <small className='text-muted'>Last updated 3 mins ago</small>
                 </Card.Footer> */}
-                </Card>
-              </div>
-            );
-          })}
+              </Card>
+            </div>
+          );
+        })}
       </CardGroup>
       <ModalMovie
         now={now}
@@ -43,6 +71,7 @@ function MovieList(props) {
         handleClose={() => {
           setShow(false);
         }}
+        updateReview={props.updateReview}
       />
     </>
   );
